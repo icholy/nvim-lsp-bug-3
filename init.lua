249c87fea3cf45eb1d4c766ffe29f06783dd626c
Ilia Choly <ilia.choly@gmail.com>
2024-05-04 22:16:44 -0400

    fix config

--- init.lua
+++ init.lua
@@ -9,8 +9,5 @@ lspconfig.gopls.setup({})
 -- we don't want this LSP to do anything, but we need 2 attached
 -- for the bug to trigger
 lspconfig.gopls2.setup({
-	capabilities = {},
-	on_attach = function (client)
-		client.server_capabilities = {}
-	end
+	capabilities = {}
 })
