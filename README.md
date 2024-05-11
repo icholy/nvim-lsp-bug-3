351f75477244dfe885a3c9ab278174a086cf1eef
Ilia Choly <ilia.choly@gmail.com>
2024-05-11 14:25:14 -0400

    improve repro

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 * `go install golang.org/x/tools/gopls@latest`
 * `go install github.com/icholy/dummylsp@latest`
 * `nvim --clean -u init.lua test.go`
-* `:!echo " package test" > test.go`
-* `:lua vim.lsp.buf.format()`
+* `:!echo " " >> test.go`
 * `:lua vim.lsp.buf.format()`
--- init.lua
+++ init.lua
@@ -1,16 +1,11 @@
+local gopls = vim.lsp.start_client({ name = "gopls", cmd = { "gopls", "serve" } })
+local dummy = vim.lsp.start_client({ name = "dummy", cmd = { "dummylsp" } })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "go" },
 	callback = function()
 		local bufnr = vim.api.nvim_get_current_buf()
-		vim.lsp.buf_attach_client(bufnr, vim.lsp.start_client({
-			name = "gopls",
-			cmd = { "gopls", "serve" },
-		}))
-		vim.lsp.buf_attach_client(bufnr, vim.lsp.start_client({
-			name = "dummy",
-			cmd = { "dummylsp" },
-		}))
-		return true
+		vim.lsp.buf_attach_client(bufnr, gopls)
+		vim.lsp.buf_attach_client(bufnr, dummy)
 	end
 })
